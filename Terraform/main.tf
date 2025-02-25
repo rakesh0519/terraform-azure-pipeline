9f3be1482a26fb87d1a5c08c35bb7ff1862fdd51
rakesh0519 <rakirakesh092@icloud.com>
2025-02-25 18:18:18 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,12 +2,10 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_key_vault" "keyvault" {
-  name                = var.key_vault_name
+resource "azurerm_key_vault" "example" {
+  name                = "my-keyvault-terraform"
+  location            = "East US"
   resource_group_name = var.resource_group_name
-  location           = var.location
-  sku_name           = "standard"
-  tenant_id          = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+  tenant_id           = var.tenant_id
 }
-
-data "azurerm_client_config" "current" {}
