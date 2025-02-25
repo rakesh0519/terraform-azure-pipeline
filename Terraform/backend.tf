16c50390e6dbd0d12f764616113d15e717d16d93
rakesh0519 <rakirakesh092@icloud.com>
2025-02-25 18:24:35 +0530

    Delete Terraform/backend.tf

--- Terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-#terraform {
-#  backend "azurerm" {
-#    resource_group_name   = "test-resourcegroup"
-#    storage_account_name  = "devopsstorage12345"
-#    container_name        = "tfstate"
-#    key                   = "terraform.tfstate"
-#  }
-#}
