499e5cde6e190af26c7c73fef48054fc9b2719a2
rakesh0519 <rakirakesh092@icloud.com>
2025-02-25 18:38:26 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,13 +2,18 @@ provider "azurerm" {
   features {}
 }
 
-terraform {
-  backend "local" {}  # This stores the Terraform state locally
+resource "azurerm_storage_account" "example" {
+  name                     = "tfstateexample123" # Change this to a unique name
+  resource_group_name      = "test-resourcegroup"
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 resource "azurerm_key_vault" "example" {
-  name                = "my-keyvault-terraform"
-  location            = "East US"
-  resource_group_name = var.resource_group_name
-  sku_name            = "standard"
+  name                        = "example-keyvault-123"
+  resource_group_name         = "test-resourcegroup"
+  location                    = "East US"
+  sku_name                    = "standard"
+  tenant_id                   = "48b4641a-04b1-4c42-95c9-757110f1641a"  # Replace with actual tenant ID
 }
