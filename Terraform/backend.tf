e96e4059f97596f1f2ecba66073f6e95bde4564c
rakesh0519 <rakirakesh092@icloud.com>
2025-02-25 18:14:20 +0530

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "test-resourcegroup"
-    storage_account_name  = "devopsstorage12345"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
+#terraform {
+#  backend "azurerm" {
+#    resource_group_name   = "test-resourcegroup"
+#    storage_account_name  = "devopsstorage12345"
+#    container_name        = "tfstate"
+#    key                   = "terraform.tfstate"
+#  }
+#}
